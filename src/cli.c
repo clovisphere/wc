59ee096ebc1d81e558356c5b14778f0d13af4404
clovisphere <clovis.mugaruka@gmail.com>
2023-08-31 12:15:40 +0300

    Add logic to get total stats when we have more than 1 file + cleanup

--- src/cli.c
+++ src/cli.c
@@ -8,9 +8,7 @@
 #include <unistd.h>
 
 // prints the executable version
-void version(char *exec) {
-  printf("%s(1) - version %s\n", exec, WC_VERSION);
-}
+void version(char *exec) { printf("%s(1) - version %s\n", exec, WC_VERSION); }
 
 // starts program execution
 int main(int argc, char **argv) {
@@ -40,10 +38,26 @@ int main(int argc, char **argv) {
   // If we did things well, the below code is all we need //
   // to display (wc) results                              //
   /* **************************************************** */
+
+  // hiold the total stats when we have more than 1 file.
+  wc total = {0, 0, 0};
+
   for (int i = optind; i < argc; i++) {
     wc stat = {0, 0, 0};
     generate_count(argv[i], &stat);
     report(argv[i], stat, option);
+    /* ******************************************************************** */
+    //  when getting stats for more than one file                           //
+    //  we need to calculate the overall wc i.e for lines, word, and bytes  //
+    /* ******************************************************************** */
+    total.line += stat.line;
+    total.word += stat.word;
+    total.bytes += stat.bytes;
+  }
+  if (option && argc > 2) {
+    printf("%-50s\n", SEPARATOR);
+    printf("%4s%7d %7d %7d %-15s%2s\n", " 😽", total.line, total.word,
+           total.bytes, "[total]", "🧮");
   }
   return EXIT_SUCCESS;
 }
